d829d7a34680fa88bec32c803778d15493ae5591
marcelo-souto <marcelosouto676@gmail.com>
2024-06-13 17:23:45 -0300

    feat: app initializer

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [ConfigModule, DatabaseModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
